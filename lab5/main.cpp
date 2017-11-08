0ecb7794b974de04cfa8ea75af2110b8317f7d00
Evgenius2020 <spleen47@mail.ru>
2017-11-08 14:05:27 +0700

    lab-5 Builing scripts

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -1,5 +1,24 @@
 #include <opencv2/highgui/highgui.hpp>
 
+IplImage *ac1(IplImage *image)
+{
+    int i = 0;
+    for (int y = 0; y < image->height; y++)
+    {
+        uchar *ptr = (uchar *)(image->imageData +
+                               y * image->widthStep);
+        for (int x = 0; x < image->width; x++)
+        {
+            ptr[3 * x] += i;
+            ptr[3 * x + 1] += i;
+            ptr[3 * x + 2] += i;
+        }
+        i += 15;
+        i %= 256;
+    }
+    return image;
+}
+
 int main(int argc, char *argv[])
 {
     CvCapture *capture = cvCreateCameraCapture(0);
@@ -10,7 +29,7 @@ int main(int argc, char *argv[])
         IplImage *frame = cvQueryFrame(capture);
         if (!frame)
             break;
-        cvShowImage("test", frame);
+        cvShowImage("test", ac1(cvCloneImage(frame)));
         char c = cvWaitKey(33);
         if (c == 27)
             break;
