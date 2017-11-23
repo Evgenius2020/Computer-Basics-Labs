7692b267c86038771433dadafd7f8d4d7ddb20bd
Evgenius2020 <spleen47@mail.ru>
2017-11-23 21:19:58 +0700

    lab-6 Getting serial number

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -1,13 +1,15 @@
 #include <libusb-1.0/libusb.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    libusb_device **devs; //  devices list
+    libusb_device **devs;       //  devices list
     libusb_context *ctx = NULL; // libusb session context
+    unsigned char *buf = (unsigned char *)malloc(256);
     int res;
     res = libusb_init(&ctx);
-    if (res < 0) 
+    if (res < 0)
     {
         fprintf(stderr, "Ошибка: инициализация не выполнена, код: %d.\n", res);
         return 1;
@@ -19,25 +21,29 @@ int main()
                 "Ошибка: список USB устройств не получен.\n");
         return 1;
     }
-    printf("Total devices: %d\n", cnt);
-    printf("Class Vendor_ID Product_ID:\n");
+    printf("Total devices: %ld\n", cnt);
+    printf("Class Vendor_ID Product_ID Serial_Number:\n");
     for (size_t i = 0; i < cnt; i++)
-    {                     
-        libusb_device_descriptor desc;    // дескриптор устройства
+    {
+        libusb_device_descriptor desc;
         res = libusb_get_device_descriptor(devs[i], &desc);
         if (res < 0)
         {
             fprintf(stderr,
                     "Ошибка: дескриптор устройства не получен, код: %d.\n", res);
-            return;
+            return 1;
         }
-        printf("%.5d %.9d %.10d\n",
-            (int)desc.bDeviceClass,
-            desc.idVendor,
-            desc.idProduct
-        );
+        libusb_device_handle *devHandle = libusb_open_device_with_vid_pid(ctx, desc.idVendor, desc.idProduct);
+        libusb_get_string_descriptor_ascii(devHandle, desc.iSerialNumber, buf, 256);
+        printf("%.5d %.9d %.10d %s\n",
+               (int)desc.bDeviceClass,
+               desc.idVendor,
+               desc.idProduct,
+               buf
+               );
     }
     libusb_free_device_list(devs, 1);
     libusb_exit(ctx);
-     return 0;
+    free(buf);
+    return 0;
 }
